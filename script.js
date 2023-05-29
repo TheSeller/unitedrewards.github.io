194a8a156db451163423f4f63199eff99a61e6e7
TheSeller <44444835+TheSeller@users.noreply.github.com>
2023-05-29 12:22:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-// Get the displayed-text element
-var displayedTextElement = document.getElementById("displayed-text");
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  // Generate a random root URL
+  var rootUrl = generateRootUrl();
+  
+  // Get the entered text
+  var textInput = document.getElementById("textInput").value;
+  
+  // Save the text and root URL to local storage or a database of your choice
+  
+  // Display the root URL to the user
+  document.getElementById("rootUrl").textContent = rootUrl;
+});
 
-// Extract the text from the URL
-var url = window.location.href;
-var text = url.substr(url.lastIndexOf('/') + 1);
-
-// Display the text on the page
-displayedTextElement.textContent = decodeURI(text);
+// Function to generate a random root URL
+function generateRootUrl() {
+  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var length = 10;
+  var rootUrl = "";
+  
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    rootUrl += characters.charAt(randomIndex);
+  }
+  
+  return rootUrl;
+}
